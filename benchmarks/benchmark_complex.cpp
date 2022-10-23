2eb6a021c457ce30bb50190d251cc3a7823481eb
Theodor <ftri2001@gmail.com>
2022-10-23 21:11:55 +0300

    Added a few more metrics + bug fixes
    Co-authored-by: lithium1337mwfgtkam <lithium1337mwfgtkam@users.noreply.github.com>

--- benchmarks/benchmark_complex.cpp
+++ benchmarks/benchmark_complex.cpp
@@ -6,6 +6,7 @@
 #include <algorithm>
 #include <cstdint>
 #include <iostream>
+#include <limits>
 
 class Worker
 {
@@ -37,28 +38,17 @@ public:
         COUNT
     };
 
-    Worker(uint32_t t_totalOps = 1024 * 128,
-           uint32_t t_totalAllocOps = std::numeric_limits<uint32_t>::max(),
-           uint32_t t_totalFreeOps = std::numeric_limits<uint32_t>::max(),
+    Worker(benchmark::State& t_bmState,
+           uint32_t t_totalOps = 1024 * 128,
            std::array<std::array<float, 4>, 4> t_transitionMatrix = m_defaultTransitionMatrix,
-           int64_t t_maxMemoryConsumption = (int64_t)1024 * 1024 * 1024 * 1 /* 1024 Mb */,
+           int64_t t_maxMemoryConsumption = (int64_t)1024 * 1024 * 1024 * 2 /* 1024 Mb */,
            uint64_t t_xorshiftSeed = 0x133796A5FF21B3C1)
-        : m_totalOps(t_totalOps)
+        : m_bmState(t_bmState)
+        , m_activePtrs(20'000, nullptr)
+        , m_totalOps(t_totalOps)
         , m_maxMemoryConsumption(t_maxMemoryConsumption)
         , m_transitionMatrix(t_transitionMatrix)
     {
-        if (t_totalAllocOps == std::numeric_limits<uint32_t>::max())
-            m_totalAllocOps = t_totalOps * 3 / 5;
-        else
-            m_totalAllocOps = t_totalAllocOps;
-
-        if (t_totalFreeOps == std::numeric_limits<uint32_t>::max())
-            m_totalFreeOps = t_totalOps * 2 / 5;
-        else
-            m_totalFreeOps = t_totalFreeOps;
-
-        m_activePtrs = std::move(std::vector<void*>(t_totalOps, nullptr));
-
         m_transitionsState = bm::utils::XorshiftNext(t_xorshiftSeed);
         m_sizesState = bm::utils::XorshiftNext(t_xorshiftSeed);
 
@@ -66,9 +56,12 @@ public:
         CalculateScatters();
     }
 
-    std::tuple<uint32_t, uint32_t, uint32_t> RunBenchmark()
+    std::tuple<uint32_t, uint32_t, uint32_t, int32_t> RunBenchmark()
     {
-        while (m_ops++ <= m_totalOps) {
+        m_bmState.PauseTiming();
+
+        while (m_allocOps + m_freeOps <= m_totalOps) {
+            ++m_ops;
             auto op = GetNextOperation();
             switch (op) {
                 case Operation::ALLOC_SINGLE:
@@ -92,19 +85,9 @@ public:
                 default:
                     break;
             }
-
-            // std::cout << m_ops++ << ". "
-            //           << "m_allocOps: " << m_allocOps << ". m_freeOps: " << m_freeOps <<
-            //           std::endl;
-            // std::cout << m_totalAllocated << std::endl;
-
-            // if ((m_allocOps >= m_totalAllocOps && m_freeOps >= m_totalFreeOps))
-            //     break;
-
-            // m_ops++;
         }
 
-        return { m_ops, m_allocOps, m_freeOps };
+        return { m_ops, m_allocOps, m_freeOps, m_maxActivePtrs };
     }
 
     void CleanUp()
@@ -119,13 +102,13 @@ public:
 
 private:
     std::vector<void*> m_activePtrs;
-    uint32_t m_totalAllocOps;
-    uint32_t m_totalFreeOps;
     uint32_t m_totalOps;
 
     uint32_t m_allocOps = 0;
     uint32_t m_freeOps = 0;
     uint32_t m_ops = 0;
+    int32_t m_maxActivePtrs = std::numeric_limits<int32_t>::min();
+    int32_t m_currActivePtrs = 0;
 
     int64_t m_maxMemoryConsumption;
     int64_t m_totalAllocated = 0;
@@ -140,6 +123,8 @@ private:
     uint32_t m_allocIdx = 0;
     uint64_t m_allocOpsIdx = 0;
     uint64_t m_freeOpsIdx = 0;
+
+    benchmark::State& m_bmState;
 
     Operation m_currentOp = Operation::ALLOC_SINGLE;
     std::array<std::array<float, 4>, 4> m_transitionMatrix;
@@ -184,11 +169,11 @@ private:
     {
         uint32_t primeIdx = 0;
         m_allocScatter = g_Primes[primeIdx];
-        while ((m_totalAllocOps % m_allocScatter) == 0)
+        while ((m_activePtrs.size() % m_allocScatter) == 0)
             m_allocScatter = g_Primes[++primeIdx];
 
         m_freeScatter = g_Primes[++primeIdx];
-        while ((m_totalAllocOps % m_freeScatter) == 0)
+        while ((m_activePtrs.size() % m_freeScatter) == 0)
             m_freeScatter = g_Primes[++primeIdx];
     }
 
@@ -235,28 +220,36 @@ private:
 
     inline void AllocSingle(int64_t t_size)
     {
-        if (m_totalAllocated >= m_maxMemoryConsumption /* || m_allocOps >= m_totalAllocOps */)
+        if (m_totalAllocated >= m_maxMemoryConsumption)
             return;
+
+        m_bmState.ResumeTiming();
 
         if (m_activePtrs[m_allocIdx]) {
             m_totalAllocated -= *static_cast<int64_t*>(m_activePtrs[m_allocIdx]);
             BenchmarkDeallocate(m_activePtrs[m_allocIdx]);
             m_activePtrs[m_allocIdx] = nullptr;
-            m_freeOps++;
+            ++m_freeOps;
+            --m_currActivePtrs;
         }
 
         m_activePtrs[m_allocIdx] = BenchmarkAllocate(t_size);
+        m_bmState.PauseTiming();
         *static_cast<int64_t*>(m_activePtrs[m_allocIdx]) = t_size;
 
         m_allocIdx = (m_allocIdx + m_allocScatter) % m_activePtrs.size();
         m_totalAllocated += t_size;
 
         ++m_allocOps;
+        ++m_currActivePtrs;
+
+        if (m_currActivePtrs > m_maxActivePtrs)
+            m_maxActivePtrs = m_currActivePtrs;
     }
 
     inline void AllocMany(int32_t t_total, bool t_randomSizes = true)
     {
-        if (m_totalAllocated >= m_maxMemoryConsumption /* || m_allocOps >= m_totalAllocOps */)
+        if (m_totalAllocated >= m_maxMemoryConsumption)
             return;
 
         // std::cout << "AllocMany(" << t_total << ", " << t_randomSizes << ")" << std::endl;
@@ -271,15 +264,14 @@ private:
 
     inline void FreeSingle()
     {
-        // if (m_freeOps >= m_totalFreeOps)
-        //     return;
-
-        // std::cout << "FreeSingle()" << std::endl;
         if (m_activePtrs[m_freeIdx]) {
             m_totalAllocated -= *static_cast<int64_t*>(m_activePtrs[m_freeIdx]);
+            m_bmState.ResumeTiming();
             BenchmarkDeallocate(m_activePtrs[m_freeIdx]);
+            m_bmState.PauseTiming();
             m_activePtrs[m_freeIdx] = nullptr;
             ++m_freeOps;
+            --m_currActivePtrs;
         }
 
         m_freeIdx = (m_freeIdx + m_freeScatter) % m_activePtrs.size();
@@ -301,13 +293,11 @@ static void BM_Complex(benchmark::State& state, Args&&... args)
 {
     auto argsTuple = std::make_tuple(std::move(args)...);
     auto totalOps = std::get<0>(argsTuple);
-    auto totalAllocOps = std::get<1>(argsTuple);
-    auto totalFreeOps = std::get<2>(argsTuple);
-    auto transitionMatrix = std::get<3>(argsTuple);
+    auto transitionMatrix = std::get<1>(argsTuple);
 
     for (auto _ : state) {
         state.PauseTiming();
-        Worker worker(totalOps, totalAllocOps, totalFreeOps, transitionMatrix);
+        Worker worker(state, totalOps, transitionMatrix);
         state.ResumeTiming();
 
         auto result = worker.RunBenchmark();
@@ -317,105 +307,27 @@ static void BM_Complex(benchmark::State& state, Args&&... args)
         state.counters["TotalOperations"] = std::get<0>(result);
         state.counters["TotalAllocOperations"] = std::get<1>(result);
         state.counters["TotalFreeOperations"] = std::get<2>(result);
+        state.counters["MaxActivePtrs"] = std::get<3>(result);
         state.ResumeTiming();
     }
 }
 
-// BENCHMARK_CAPTURE(
-//     BM_Complex,
-//     "Total ops: 131072, Relative alloc ops: 0.8, Relative free ops: 0.2, Transition matrix:
-//     ver-1", 2 << 16, (2 << 16) * 4 / 5, (2 << 16) * 1 / 5, std::array<std::array<float, 4>, 4>{
-//         std::array<float, 4>{ 0.15, 0.0, 0.0, 0.85 },   // AllocateSingle
-//         std::array<float, 4>{ 0.65, 0.0, 0.35, 0.0 },   // DeallocateSingle
-//         std::array<float, 4>{ 0.0, 0.82, 0.0, 0.18 },   // AllocateMultiple
-//         std::array<float, 4>{ 0.07, 0.00, 0.93, 0.00 }, // DeallocateMultiple
-//     })
-//     ->Unit(benchmark::kMillisecond);
-
-// BENCHMARK_CAPTURE(
-//     BM_Complex,
-//     "Total ops: 131072, Relative alloc ops: 0.8, Relative free ops: 0.2, Transition matrix:
-//     ver-2", (2 << 16), (2 << 16) * 4 / 5, (2 << 16) * 1 / 5, std::array<std::array<float, 4>, 4>{
-//         std::array<float, 4>{ 0.2, 0.1, 0.6, 0.1 },   // AllocateSingle
-//         std::array<float, 4>{ 0.4, 0.1, 0.3, 0.2 },   // DeallocateSingle
-//         std::array<float, 4>{ 0.1, 0.4, 0.1, 0.4 },   // AllocateMultiple
-//         std::array<float, 4>{ 0.5, 0.05, 0.4, 0.05 }, // DeallocateMultiple
-//     })
-//     ->Unit(benchmark::kMillisecond);
-
-// BENCHMARK_CAPTURE(
-//     BM_Complex,
-//     "Total ops: 131072, Relative alloc ops: 0.6, Relative free ops: 0.4, Transition matrix:
-//     ver-1", 2 << 16, (2 << 16) * 3 / 5, (2 << 16) * 2 / 5, std::array<std::array<float, 4>, 4>{
-//         std::array<float, 4>{ 0.15, 0.0, 0.0, 0.85 },   // AllocateSingle
-//         std::array<float, 4>{ 0.65, 0.0, 0.35, 0.0 },   // DeallocateSingle
-//         std::array<float, 4>{ 0.0, 0.82, 0.0, 0.18 },   // AllocateMultiple
-//         std::array<float, 4>{ 0.07, 0.00, 0.93, 0.00 }, // DeallocateMultiple
-//     })
-//     ->Unit(benchmark::kMillisecond);
-
-// BENCHMARK_CAPTURE(
-//     BM_Complex,
-//     "Total ops: 131072, Relative alloc ops: 0.6, Relative free ops: 0.4, Transition matrix:
-//     ver-2", (2 << 16), (2 << 16) * 3 / 5, (2 << 16) * 2 / 5, std::array<std::array<float, 4>, 4>{
-//         std::array<float, 4>{ 0.2, 0.1, 0.6, 0.1 },   // AllocateSingle
-//         std::array<float, 4>{ 0.4, 0.1, 0.3, 0.2 },   // DeallocateSingle
-//         std::array<float, 4>{ 0.1, 0.4, 0.1, 0.4 },   // AllocateMultiple
-//         std::array<float, 4>{ 0.5, 0.05, 0.4, 0.05 }, // DeallocateMultiple
-//     })
-//     ->Unit(benchmark::kMillisecond);
-
-// BENCHMARK_CAPTURE(
-//     BM_Complex,
-//     "Total ops: 131072, Relative alloc ops: 0.4, Relative free ops: 0.6, Transition matrix:
-//     ver-1", 2 << 16, (2 << 16) * 2 / 5, (2 << 16) * 3 / 5, std::array<std::array<float, 4>, 4>{
-//         std::array<float, 4>{ 0.15, 0.0, 0.0, 0.85 },   // AllocateSingle
-//         std::array<float, 4>{ 0.65, 0.0, 0.35, 0.0 },   // DeallocateSingle
-//         std::array<float, 4>{ 0.0, 0.82, 0.0, 0.18 },   // AllocateMultiple
-//         std::array<float, 4>{ 0.07, 0.00, 0.93, 0.00 }, // DeallocateMultiple
-//     })
-//     ->Unit(benchmark::kMillisecond);
-
-// BENCHMARK_CAPTURE(
-//     BM_Complex,
-//     "Total ops: 131072, Relative alloc ops: 0.4, Relative free ops: 0.6, Transition matrix:
-//     ver-2", (2 << 16), (2 << 16) * 2 / 5, (2 << 16) * 3 / 5, std::array<std::array<float, 4>, 4>{
-//         std::array<float, 4>{ 0.2, 0.1, 0.6, 0.1 },   // AllocateSingle
-//         std::array<float, 4>{ 0.4, 0.1, 0.3, 0.2 },   // DeallocateSingle
-//         std::array<float, 4>{ 0.1, 0.4, 0.1, 0.4 },   // AllocateMultiple
-//         std::array<float, 4>{ 0.5, 0.05, 0.4, 0.05 }, // DeallocateMultiple
-//     })
-//     ->Unit(benchmark::kMillisecond);
-
-// BENCHMARK_CAPTURE(
-//     BM_Complex,
-//     "Total ops: 131072, Relative alloc ops: 0.2, Relative free ops: 0.8, Transition matrix:
-//     ver-1", 2 << 16, (2 << 16) * 1 / 5, (2 << 16) * 4 / 5, std::array<std::array<float, 4>, 4>{
-//         std::array<float, 4>{ 0.15, 0.0, 0.0, 0.85 },   // AllocateSingle
-//         std::array<float, 4>{ 0.65, 0.0, 0.35, 0.0 },   // DeallocateSingle
-//         std::array<float, 4>{ 0.0, 0.82, 0.0, 0.18 },   // AllocateMultiple
-//         std::array<float, 4>{ 0.07, 0.00, 0.93, 0.00 }, // DeallocateMultiple
-//     })
-//     ->Unit(benchmark::kMillisecond);
-
-// BENCHMARK_CAPTURE(
-//     BM_Complex,
-//     "Total ops: 131072, Relative alloc ops: 0.2, Relative free ops: 0.8, Transition matrix:
-//     ver-2", (2 << 16), (2 << 16) * 1 / 5, (2 << 16) * 4 / 5, std::array<std::array<float, 4>, 4>{
-//         std::array<float, 4>{ 0.2, 0.1, 0.6, 0.1 },   // AllocateSingle
-//         std::array<float, 4>{ 0.4, 0.1, 0.3, 0.2 },   // DeallocateSingle
-//         std::array<float, 4>{ 0.1, 0.4, 0.1, 0.4 },   // AllocateMultiple
-//         std::array<float, 4>{ 0.5, 0.05, 0.4, 0.05 }, // DeallocateMultiple
-//     })
-//     ->Unit(benchmark::kMillisecond);
-
-#define BENCHMARK_ITER(iters)                                                                      \
+#define BENCHMARK_MAT1(iters)                                                                      \
     BENCHMARK_CAPTURE(BM_Complex,                                                                  \
-                      "Total ops: " #iters " , Relative alloc ops: 0.6, Relative free ops: 0.4, "  \
-                      "Transition matrix: ver-2",                                                  \
+                      "Total ops: " #iters "Transition matrix: ver-1",                             \
                       (iters),                                                                     \
-                      (iters)*1 / 2,                                                               \
-                      (iters)*1 / 2,                                                               \
+                      std::array<std::array<float, 4>, 4>{                                         \
+                          std::array<float, 4>{ 0.2, 0.1, 0.6, 0.1 },   /* AllocateSingle */       \
+                          std::array<float, 4>{ 0.4, 0.1, 0.3, 0.2 },   /* DeallocateSingle */     \
+                          std::array<float, 4>{ 0.1, 0.4, 0.1, 0.4 },   /* AllocateMultiple */     \
+                          std::array<float, 4>{ 0.5, 0.05, 0.4, 0.05 }, /* DeallocateMultiple */   \
+                      })                                                                           \
+        ->Unit(benchmark::kMillisecond);
+
+#define BENCHMARK_MAT2(iters)                                                                      \
+    BENCHMARK_CAPTURE(BM_Complex,                                                                  \
+                      "Total ops: " #iters "Transition matrix: ver-2",                             \
+                      (iters),                                                                     \
                       std::array<std::array<float, 4>, 4>{                                         \
                           std::array<float, 4>{ 0.15, 0.0, 0.0, 0.85 },   /* AllocateSingle */     \
                           std::array<float, 4>{ 0.65, 0.0, 0.35, 0.0 },   /* DeallocateSingle */   \
@@ -424,18 +336,14 @@ static void BM_Complex(benchmark::State& state, Args&&... args)
                       })                                                                           \
         ->Unit(benchmark::kMillisecond);
 
-BENCHMARK_ITER(100'000);
-BENCHMARK_ITER(200'000);
-BENCHMARK_ITER(300'000);
-BENCHMARK_ITER(400'000);
-BENCHMARK_ITER(500'000);
-BENCHMARK_ITER(600'000);
-BENCHMARK_ITER(700'000);
-BENCHMARK_ITER(800'000);
-BENCHMARK_ITER(900'000);
-BENCHMARK_ITER(1'000'000);
-BENCHMARK_ITER(1'100'000);
-BENCHMARK_ITER(1'200'000);
-BENCHMARK_ITER(1'300'000);
-BENCHMARK_ITER(1'400'000);
-BENCHMARK_ITER(1'500'000);
+BENCHMARK_MAT1(200'000);
+BENCHMARK_MAT1(400'000);
+BENCHMARK_MAT1(600'000);
+BENCHMARK_MAT1(800'000);
+BENCHMARK_MAT1(1'000'000);
+BENCHMARK_MAT1(1'200'000);
+BENCHMARK_MAT1(1'400'000);
+BENCHMARK_MAT1(1'400'000);
+BENCHMARK_MAT1(1'600'000);
+BENCHMARK_MAT1(1'800'000);
+BENCHMARK_MAT1(2'000'000);
